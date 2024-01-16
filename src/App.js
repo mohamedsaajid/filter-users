a8601118dc0a35ba021ef00c1eda2adab27d2e14
Saajid <117673036+mohamedsaajid@users.noreply.github.com>
2024-01-16 16:30:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ChipInput from './components/ChipInput.tsx'; 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Pick Users</h1>
       </header>
+      <main>
+        <ChipInput initialItems={['Soham', 'Ken', 'Denise', 'Tracy', 'Brad','Calvin']} />
+      </main>
     </div>
   );
 }
